55eb040f7d49cccec512a6093a7a6039cdf75a49
Alfir Tyakin <alfir777@rambler.ru>
2022-03-30 18:21:48 +0500

    add for admin panel

--- config/core/admin.py
+++ config/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import URL, Profile
+
+
+class URLAdminForm(forms.ModelForm):
+    class Meta:
+        model = URL
+        fields = '__all__'
+
+
+@admin.register(URL)
+class URLAdmin(admin.ModelAdmin):
+    form = URLAdminForm
+    list_display = ('id', 'full_url', 'short_url', 'clicks', 'created_at')
